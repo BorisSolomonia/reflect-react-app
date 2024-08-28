1ef817732f7b1ccc8ea95e6895a5e7c2a67236f0
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2024-08-28 23:01:06 +0400

    13

--- src/Home_Profile.js
+++ src/Home_Profile.js
@@ -201,7 +201,7 @@ function HomeProfile() {
       <HeroContent>
         <Container maxWidth="sm">
           <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-            Welcome to Reflection
+            Welcome Dojiki
           </Typography>
           <Typography variant="h5" align="center" color="textSecondary" paragraph>
             Share your personal notes, thoughts, and experiences at specific locations. Connect with others through shared memories and insights.
